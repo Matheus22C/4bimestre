9459e700afdfa5e7225a85b1f38b25dc639ae143
Matheus22C Matheus22C <matheusteteuabreu@gmail.com>
2024-10-10 11:08:08 +0000

    arrumado

---
+++ script.js
@@ -0,0 +1,25 @@
+const input = require("prompt-sync")();
+
+const alcool = 0
+const gasolina = 0
+const diesel = 0
+const resposta = 0
+
+while (resposta !== 4) {
+      resposta = Number(input("escolha seu combustível"))
+      if (resposta === 1) {
+            alcool++
+      } else if(resposta === 2) {
+            gasolina++
+      } else if(resposta === 3) {
+            diesel++
+      }
+}
+
+console.log("muito obrigado")
+
+console.log(`Álcool: ${alcool}`)
+
+console.log(`Gasolina: ${gasolina}`)
+
+console.log(`Diesel: ${diesel}`)
